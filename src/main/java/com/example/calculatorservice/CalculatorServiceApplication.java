c5f8bb135c23f9a19f555b65cdcf1658e9632044
David Christa <davidchrista@gmail.com>
2020-04-16 09:59:33 +0200

    Revert "change to war"
    This reverts commit eb1f404f7c86ab8cb0bda817fc9c59242260e6a6.

--- src/main/java/com/example/calculatorservice/CalculatorServiceApplication.java
+++ src/main/java/com/example/calculatorservice/CalculatorServiceApplication.java
@@ -2,16 +2,9 @@ package com.example.calculatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalculatorServiceApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CalculatorServiceApplication.class);
-	}
+public class CalculatorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorServiceApplication.class, args);
